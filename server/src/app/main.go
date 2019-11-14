87b073524ede2ec7fd690480239a8a129d101920
Yasukastsu.A <handbuuuu@gmail.com>
2019-11-15 00:28:54 +0900

    fix main.go

--- server/src/app/main.go
+++ server/src/app/main.go
@@ -1,1 +1,24 @@
 package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func public(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("hello public!\n"))
+}
+
+func private(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("hello private!\n"))
+}
+
+func main() {
+	r := mux.NewRouter()
+	r.HandleFunc("/public", public)
+	r.HandleFunc("/private", private)
+
+	log.Fatal(http.ListenAndServe(":8000", r))
+}
