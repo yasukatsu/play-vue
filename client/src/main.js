ad7ec149258464ae613305b9a1043075997fe62e
Yasukastsu.A <handbuuuu@gmail.com>
2019-11-16 14:31:23 +0900

    fix firebase

--- client/src/main.js
+++ client/src/main.js
@@ -3,8 +3,19 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import firebase from 'firebase'
 
 Vue.config.productionTip = false
+
+const config = {
+  apiKey: 'AIzaSyBDepWRx-2QiNriZif3dGmLm1CNfvk0b_k',
+  authDomain: 'play-vue-201911.firebaseapp.com',
+  databaseURL: 'https://play-vue-201911.firebaseio.com',
+  projectId: 'play-vue-201911',
+  storageBucket: 'play-vue-201911.appspot.com',
+  messagingSenderId: '783440684054'
+}
+firebase.initializeApp(config)
 
 /* eslint-disable no-new */
 new Vue({
