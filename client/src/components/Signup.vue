2ccff6abd6acd3b19c27c035c042527a16719012
Yasukastsu.A <handbuuuu@gmail.com>
2019-11-16 15:11:54 +0900

    fix signup

--- client/src/components/Signup.vue
+++ client/src/components/Signup.vue
@@ -1,22 +1,32 @@
 <template>
-    <div class="signup">
-        <h2>Sign up</h2>
-        <input type="text" placeholder="Username" v-model="email">
-        <input type="password" placeholder="Password" v-model="password">
-        <button>Register</button>
-        <p>Do you have an account?
-            <router-link to="/signin">sign in now!!</router-link>
-        </p>
-    </div>
+  <div class="signup">
+    <h2>Sign up</h2>
+    <input type="text" placeholder="Username" v-model="email">
+    <input type="password" placeholder="Password" v-model="password">
+    <button @click="signUp">Register</button>
+    <p>Do you have an account?
+      <router-link to="/signin">sign in now!!</router-link>
+    </p>
+  </div>
 </template>
 
 <script>
+import firebase from 'firebase'
 export default {
   name: 'Signup',
   data () {
     return {
       email: '',
       password: ''
+    }
+  },
+  methods: {
+    signUp: function () {
+      firebase.auth().createUserWithEmailAndPassword(this.email, this.password).then(res => {
+        console.log('Create account: ', res.user.email)
+      }).catch(error => {
+        console.log(error.message)
+      })
     }
   }
 }
