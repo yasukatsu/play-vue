f130ed47c615b24e0efa81d37c4890632f91e615
Yasukastsu.A <handbuuuu@gmail.com>
2019-12-10 01:03:58 +0900

    firebaseAdmin & jwt

--- server/src/app/main.go
+++ server/src/app/main.go
@@ -1,11 +1,17 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"strings"
 
+	firebase "firebase.google.com/go"
 	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
+	"google.golang.org/api/option"
 )
 
 func public(w http.ResponseWriter, r *http.Request) {
@@ -23,7 +29,40 @@ func main() {
 
 	r := mux.NewRouter()
 	r.HandleFunc("/public", public)
-	r.HandleFunc("/private", private)
+	r.HandleFunc("/private", authMiddleware(private))
 
 	log.Fatal(http.ListenAndServe(":8000", handlers.CORS(allowedOrigins, allowedMethods, allowedHeaders)(r)))
+}
+
+func authMiddleware(next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		// Firebase SDK のセットアップ
+		opt := option.WithCredentialsFile(os.Getenv("GOOGLE_APPLICATION_CREDENTIALS"))
+		app, err := firebase.NewApp(context.Background(), nil, opt)
+		if err != nil {
+			fmt.Printf("error: %v\n", err)
+			os.Exit(1)
+		}
+		auth, err := app.Auth(context.Background())
+		if err != nil {
+			fmt.Printf("error: %v\n", err)
+			os.Exit(1)
+		}
+
+		// クライアントから送られてきた JWT 取得
+		authHeader := r.Header.Get("Authorization")
+		idToken := strings.Replace(authHeader, "Bearer ", "", 1)
+
+		// JWT の検証
+		token, err := auth.VerifyIDToken(context.Background(), idToken)
+		if err != nil {
+			// JWT が無効なら Handler に進まず別処理
+			fmt.Printf("error verifying ID token: %v\n", err)
+			w.WriteHeader(http.StatusUnauthorized)
+			w.Write([]byte("error verifying ID token\n"))
+			return
+		}
+		fmt.Printf("Verified ID token: %v\n", token)
+		next.ServeHTTP(w, r)
+	}
 }
